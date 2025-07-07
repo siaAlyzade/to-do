b633fbd8c9a828c96d5fe7daa1d894c1440c6d2e
sia <siaAlyzade@gmail.com>
2025-07-07 18:39:39 +0330

    some minor changes

--- src/Pages/Dashboard/Primary/MainApp/Svgs.jsx
+++ src/Pages/Dashboard/Primary/MainApp/Svgs.jsx
@@ -136,3 +136,21 @@ export function InviteSvg() {
     </svg>
   );
 }
+export function NavMenuSvg() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      x="0px"
+      y="0px"
+      width="24px"
+      height="24px"
+      viewBox="0,0,256,256"
+    >
+      <g fill="#ffffff">
+        <g transform="scale(5.12,5.12)">
+          <path d="M0,7.5v5h50v-5zM0,22.5v5h50v-5zM0,37.5v5h50v-5z"></path>
+        </g>
+      </g>
+    </svg>
+  );
+}
--- src/Pages/Dashboard/Primary/Sidebar/PhoneNavButton.jsx
+++ src/Pages/Dashboard/Primary/Sidebar/PhoneNavButton.jsx
@@ -1,8 +1,12 @@
+import { NavMenuSvg } from "../MainApp/Svgs";
+
 export default function PhoneNavButton({ onClick }) {
   return (
     <button
       onClick={onClick}
-      className="fixed z-10 left-0 w-1.5 h-16 bg-pinky xl:hidden"
-    ></button>
+      className="fixed z-10 left-0 p-2 bg-pinky xl:hidden"
+    >
+      <NavMenuSvg />
+    </button>
   );
 }
